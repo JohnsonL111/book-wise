07f23e0a91f93e16247f24677bd20e0b24a91ac1
Johnson Luong <jkl53@sfu.ca>
2023-12-29 15:11:31 -0800

    Add home page

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,20 @@ import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 // middleware to allow express to parse request payload
 app.use(express.json());
 
+app.use(cors())
 // middlware to handle CORS
-app.use(cors({
-    origin: 'https://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}))
+// app.use(cors({
+//     origin: 'https://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }))
 
 app.get("/", (request, response) => {
   console.log(request);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'> App</div>
+    <Routes>
+      <Route path='/' elements={<Home/>}/>
+      <Route path='/books/create' elements={<CreateBooks/>}/>
+      <Route path='/books/details/:id' elements={<ShowBook/>}/>
+      <Route path='/books/edit/:id' elements={<EditBook/>}/>
+      <Route path='/books/delete/:id' elements={<DeleteBook/>}/>
+    </Routes>
   )
 }
 
