9e1bc37e1d538e82d800a7f5e403df041a1a5f73
Johnson Luong <jkl53@sfu.ca>
2024-01-05 22:26:27 -0800

    Add modal

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi'
+import { BiUserCircle, BiShow } from 'react-icons/bi'
 import { BsInfoCircle } from 'react-icons/bs'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { MdOutlineDelete } from 'react-icons/md'
+import { useState } from 'react'
+import BookModal from './BookModal'
 
 const BookSingleCard = ({ book }) => {
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <div
             className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'
@@ -21,10 +25,16 @@ const BookSingleCard = ({ book }) => {
             </div>
 
             <div className='flex justify-start books-center gap-x-2'>
-                <h2 className='my-1'> {book.author}</h2>
                 <BiUserCircle className='text-red-300 text-2xl' />
+                <h2 className='my-1'> {book.author}</h2>
             </div>
+
             <div className='flex justify-between books-center gap-x-2 mt-4 p-4'>
+                <BiShow
+                    className='text-3xl text-blue-800 hover:text-black cursor-pointer'
+                    onClick={() => setShowModal(true)}
+                />
+
                 <Link to={`/books/details/${book.id}`}>
                     <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
                 </Link >
@@ -35,6 +45,11 @@ const BookSingleCard = ({ book }) => {
                     <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
                 </Link>
             </div>
+            {
+                showModal && (
+                    <BookModal book={book} onClose={() => setShowModal(false)} />
+                )
+            }
 
         </div>
     )
--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -6,6 +6,7 @@ const BooksCard = ({books}) => {
     return (
         <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {books.map((item) => (
+                // pass key for performance
                <BookSingleCard key={item._id} book={item}/>
             ))}
         </div>
